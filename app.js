c6fc49bbdb752899b44db36844e24e2cf69aaedb
s4ijem-san <mejiaseverywhere@gmail.com>
2026-04-16 21:56:02 -0400

    Commit 3 - corrección en app.js para poder inicializar

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+
 app.get('/', (req, res) => {
     res.send('Servidor HTTPS funcionando de forma correcta.');
 });
+
+module.exports = app;
